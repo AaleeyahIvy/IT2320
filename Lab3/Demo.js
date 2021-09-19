6bfbe304e8fb40f6aa6495353f751fafc6730778
Aaleeyah Ivy <53322518+AaleeyahIvy@users.noreply.github.com>
2021-09-19 15:10:17 -0400

    Add files via upload

---
+++ Lab3/Demo.js
@@ -0,0 +1,183 @@
+//Javascript is case sensitive
+
+/*Javascript statement end in a semicolon
+Javascript comments can span single or multiple lines*/
+
+
+console.log("console log - hello world");//log in developer tools
+var globalX = 100;//global scope
+
+function hello() //function definition
+{
+    alert("alert - hello world!");//popup window    
+}
+
+function add()//function definition
+{
+    var x = 3;
+    var y = 5;
+    var sum = x + y;
+    alert("Sum is " + sum);
+}
+
+function substract()
+{
+    var a = 1000;
+    var b = 100;
+    var diff = a - b;
+    alert("Difference is" + diff);
+}
+
+function multiply()
+{
+    var c = 25;
+    var d = 42;
+    var mult = c * d;
+    alert("The answer is: " + mult);
+}
+
+function divide()
+{
+    var e = 48;
+    var f = 12
+    var div = e / f;
+    alert("The answer is: " + div);
+}
+
+//Homework Lab- Complete Multiplication and Division functionality [check]
+
+function increment()
+{ 
+    var y = 10;//local scope - resets y everytime function is called
+    globalX = globalX+10;//can add any number to itself
+    //y = y+10;
+    y+=10;//same as y = y+10;
+    alert("x="+globalX + "y="+y); 
+
+}
+
+/* JS statements
+Conditional statements:
+If statement
+Switch statement
+Loops:
+For loop
+While loop
+Do while loop
+Break
+*/
+
+function compare()
+{
+    var charlie = 120;
+    var john = 120;
+    
+    //conditional if statement
+    
+    if (charlie > john)
+    {
+        alert("Charlie is taller");
+    }
+    else if( charlie == john)//"=" is assignment; "==" is comparison
+    {
+        alert("Charlie and John are of the same height");
+    }
+    else
+    {
+        alert("John is taller");
+    }
+
+}
+
+function IsTropical(fruit)//fruit is a variable and is an input parameter
+{
+    
+    var isTropical = false; //boolean variable: True or False
+
+    switch(fruit)
+    {
+        case "banana":
+            isTropical = true;
+            break;
+
+        case "papaya":
+            isTropical = true;
+            break;
+
+        case "tomato":
+            isTropical = false;
+            break;
+
+        case "mango":
+            isTropical = true;
+            break;
+
+        case "watermelon":
+            isTropical = true;
+            break;
+
+        default:
+            isTropical = false;            
+    }
+
+    alert("Is "+ fruit+ " tropical?"+ isTropical );
+
+}
+
+function Repeat( message )
+{
+    var counter = 0;
+    console.log("While loop:");
+    while (counter < 5)
+    {
+        console.log( message );
+        counter++; //You have to remember to increment the counter otherwise the loop will go on endlessly
+    }
+
+    counter = 0;
+    console.log("Do - While loop:");
+    do 
+    {
+        console.log( message );
+        counter++; //You have to remember to increment the counter otherwise the loop will go on endlessly
+    } while (counter < 5);
+
+    console.log("For loop:");
+    /************************************************* */
+    for(var counter=0; counter < 5; counter++)
+    {
+        console.log(message);
+    }
+}
+
+function mean() //calculates the mean of the homework numbers through a loop
+{
+    const nums = [25, 47, 98, 46, 52];
+    const anum = 5;
+    let sum = 0;
+    let mean = 0;
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        mean = sum / anum;
+    }
+    alert("The mean is: " + mean);
+}
+
+function evenNumbers() //loop through array of all even numbers then add them together
+{
+    const evennums = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64,66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 90, 92, 94, 96, 98, 100, 102, 104, 105, 106, 108, 110, 112, 114, 116, 118, 120, 122, 124, 126, 128, 130, 132, 134, 136, 138, 140, 142, 144, 146, 148, 150, 152, 154, 156, 158, 160, 162, 164, 166, 168, 170, 172, 174, 176, 178, 180, 182, 184, 186, 188, 190, 192, 194, 196, 198, 200];
+    let sum = 0;
+    for (let i = 0; i < evennums.length; i++) {
+        sum += evennums[i];
+    }
+    alert("Sum is: " + sum);
+}
+
+function fibonacci() //Loop is not needed but there to check if everything is working
+{
+    const fibonums = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181];
+    for (let i = 0; i < fibonums.length; i++) {
+        console.log(i);
+    }
+    alert(fibonums);
+}
