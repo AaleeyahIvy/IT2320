dd6b697450c982bbc841dbdf358b7284364d3367
Aaleeyah Ivy <53322518+AaleeyahIvy@users.noreply.github.com>
2021-11-15 18:34:17 -0500

    Add files via upload

---
+++ Lab 10/JQLab.js
@@ -0,0 +1,16 @@
+$(function(){
+$("#btn1").click(function(){
+    $("#1").hide();
+    $("#2").hide();
+    $("#3").hide();
+    $("#4").hide();
+    });
+
+$("#btn2").click(function(){
+    $("#1").show();
+    $("#2").show();
+    $("#3").show();
+    $("#4").show();
+    });
+
+});
